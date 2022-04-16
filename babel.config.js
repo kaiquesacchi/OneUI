2f64f606cd08a6f3b7410749447212e9ab979a6f
Kaique Sacchi <sacchikaique@gmail.com>
2022-04-16 18:23:27 -0300

    chore(typescript): added path aliases

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,22 @@
-module.exports = function(api) {
+module.exports = function (api) {
   api.cache(true);
   return {
-    presets: ['babel-preset-expo'],
+    presets: ["babel-preset-expo"],
+    plugins: [
+      [
+        require.resolve("babel-plugin-module-resolver"),
+        {
+          extensions: [".ios.js", ".android.js", ".js", ".ts", ".tsx", ".json"],
+          root: ["./"],
+          alias: {
+            "@atoms": "./src/components/atoms",
+            "@buttons": "./src/components/buttons",
+            "@focusBlock": "src/components/focusBlock",
+            "@layouts": "src/components/layouts",
+            "@toasts": "src/components/toasts",
+          }
+        }
+      ]
+    ]
   };
 };
